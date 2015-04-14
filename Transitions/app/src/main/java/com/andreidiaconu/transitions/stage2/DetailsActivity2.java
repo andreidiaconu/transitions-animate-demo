dfd7c9eef05dac503db1957f3d22dd796c905e83
Andrei Diaconu <diaconu.andrei.tudor@gmail.com>
2015-04-14 16:12:37 +0300

    Stage 2 - Transmitting coordinates from one activity to another and resizing the details image to look like the grid one. Also added a red background to the image so we can see the difference in scale type.

---
+++ Transitions/app/src/main/java/com/andreidiaconu/transitions/stage2/DetailsActivity2.java
@@ -0,0 +1,59 @@
+package com.andreidiaconu.transitions.stage2;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.andreidiaconu.transitions.R;
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by andrei on 4/14/2015.
+ */
+public class DetailsActivity2 extends Activity {
+
+    public static void start(Context from, String imageUrl, View initialView){
+        Intent intent = new Intent(from, DetailsActivity2.class);
+        intent.putExtra("imageUrl", imageUrl);
+
+        //Send the initial position as an intent parameter. A Rect contains x, y, width, height.
+        Rect initialPosition = new Rect();
+        initialView.getGlobalVisibleRect(initialPosition);
+        intent.putExtra("initialPosition", initialPosition);
+
+        from.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_2);
+        ((Toolbar) findViewById(R.id.action_bar)).setTitle("Details - Stage 2");
+
+        ImageView imageView = (ImageView) findViewById(R.id.image);
+        String imageUrl = getIntent().getStringExtra("imageUrl");
+
+        Picasso
+                .with(this)
+                .load(imageUrl)
+                .into(imageView);
+
+        //Use the position and size from previous screen and set it to the image on this screen.
+        Rect initialPosition = getIntent().getParcelableExtra("initialPosition");
+
+        //Don't know what the size of the image will be yet, so i'll just set it's size instead of scaling.
+        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) imageView.getLayoutParams();
+        layoutParams.width = initialPosition.width();
+        layoutParams.height = initialPosition.height();
+        layoutParams.topMargin = initialPosition.top;
+        layoutParams.leftMargin = initialPosition.left;
+        imageView.setLayoutParams(layoutParams);
+    }
+}
