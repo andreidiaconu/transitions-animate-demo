92ad9ba1222fe28068143373313d4138d2c99923
Andrei Diaconu <diaconu.andrei.tudor@gmail.com>
2015-04-15 12:16:03 +0300

    Stage 8 - Using custom transitions (explode and slide) for content animation and excluding status and navigation background

---
+++ Transitions/app/src/main/java/com/andreidiaconu/transitions/stage8/DetailsActivity8.java
@@ -0,0 +1,63 @@
+package com.andreidiaconu.transitions.stage8;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.view.WindowCompat;
+import android.support.v7.widget.Toolbar;
+import android.transition.Explode;
+import android.transition.Slide;
+import android.transition.Transition;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.andreidiaconu.transitions.R;
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by andrei on 4/14/2015.
+ */
+public class DetailsActivity8 extends Activity {
+
+    public static void start(Activity from, String imageUrl, View initialView){
+        Intent intent = new Intent(from, DetailsActivity8.class);
+        intent.putExtra("imageUrl", imageUrl);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            Transition transition = new Explode();
+            transition.excludeTarget(android.R.id.statusBarBackground, true);
+            transition.excludeTarget(android.R.id.navigationBarBackground, true);
+            from.getWindow().setExitTransition(transition);
+        }
+
+        Bundle animationBundle = ActivityOptionsCompat.makeSceneTransitionAnimation(from, initialView, "shared_image").toBundle();
+        ActivityCompat.startActivity(from,intent, animationBundle);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_wrap);
+        ((Toolbar) findViewById(R.id.action_bar)).setTitle("Details - Stage 7");
+
+        ImageView imageView = (ImageView) findViewById(R.id.image);
+        String imageUrl = getIntent().getStringExtra("imageUrl");
+
+        Picasso
+                .with(this)
+                .load(imageUrl)
+                .into(imageView);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            Transition transition = new Slide();
+            transition.excludeTarget(android.R.id.statusBarBackground, true);
+            transition.excludeTarget(android.R.id.navigationBarBackground, true);
+            getWindow().setEnterTransition(transition);
+        }
+    }
+
+}
